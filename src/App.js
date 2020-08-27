21bd74afb1b3924d27f5dcd843e7e38de4f2441a
gioinsfran1998 <gioinsfran98@gmail.com>
2020-08-27 16:44:10 -0400

    Update router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	NavLink,
+} from 'react-router-dom';
+import Inicio from './components/Inicio';
+import Contacto from './components/Contacto';
+import Nosotros from './components/Nosotros';
+import User from './components/User';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<Router>
+			<div className='container mt-5'>
+				<div className='btn-group'>
+					<NavLink
+						to='/'
+						className='btn btn-dark '
+						activeClassName='selected'
+					>
+						Inicio
+					</NavLink>
+					<NavLink
+						to='/nosotros'
+						className='btn btn-dark'
+						activeClassName='selected'
+					>
+						Nosotros
+					</NavLink>
+					<NavLink
+						to='/contacto'
+						className='btn btn-dark'
+						activeClassName='selected'
+					>
+						Contacto
+					</NavLink>
+				</div>
+				<hr />
+				<Switch>
+					<Route path='/nosotros/:id' exact>
+						<User />
+					</Route>
+					<Route path='/' exact>
+						<Inicio />
+					</Route>
+
+					<Route path='/contacto'>
+						<Contacto />
+					</Route>
+
+					<Route path='/nosotros'>
+						<Nosotros />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
